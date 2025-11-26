74a38a776d263e45d58be1f56c46177cbd256c88
Quan Tran <quantran@192.168.2.24>
2025-11-26 15:48:09 +0700

    Perf: Add DB indexes and Loading skeleton

---
+++ app/loading.tsx
@@ -0,0 +1,29 @@
+import { Skeleton } from "@/components/ui/skeleton"
+
+export default function Loading() {
+    return (
+        <div className="p-8 space-y-8">
+            <div className="flex justify-between items-center">
+                <div className="space-y-2">
+                    <Skeleton className="h-8 w-64" />
+                    <Skeleton className="h-4 w-96" />
+                </div>
+                <div className="flex gap-2">
+                    <Skeleton className="h-10 w-32" />
+                    <Skeleton className="h-10 w-32" />
+                </div>
+            </div>
+
+            <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+                {Array.from({ length: 4 }).map((_, i) => (
+                    <Skeleton key={i} className="h-32 rounded-xl" />
+                ))}
+            </div>
+
+            <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-7">
+                <Skeleton className="col-span-4 h-[400px] rounded-xl" />
+                <Skeleton className="col-span-3 h-[400px] rounded-xl" />
+            </div>
+        </div>
+    )
+}
